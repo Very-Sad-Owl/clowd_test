d8f6aea7d5ed986c16a8e665d0e0721bd39ee4d5
Owl <olga.mailychko@gmail.com>
2021-12-16 04:08:26 +0300

    simple start page has been set for test

--- src/main/java/com/boots/Application.java
+++ src/main/java/com/boots/Application.java
@@ -2,9 +2,18 @@ package com.boots;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Application {
+
+    @GetMapping("/")
+    public String message(){
+        return "Welcome to azure!";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
